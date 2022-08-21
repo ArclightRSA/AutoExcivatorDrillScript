8c472c663ed4e816c677ec8db921f8db6bffcaa5
ArclightRSA <armandh@parys.co.za>
2022-08-22 01:09:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,268 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        //******************************************************************
+        // -- Main
+        string main_tag = "Ex/";
+        // Piston Pistions
+        string XTag = "X/";
+        string ZTag = "Z/";
+
+        bool programSet;
+        bool programState;
+
+        bool pristonXExtract;
+        bool pristonZExtract;
+
+
+        IMyPistonBase piston;
+
+        List<IMyTerminalBlock> blocks;
+
+        List<IMyPistonBase> XPistons;
+        List<IMyPistonBase> ZPistons;
+
+
+        //Piston Variables
+        float PVelocity;
+        float PMinLimit;
+        float PMaxLimit;
+
+        public Program()
+        {
+            Echo("Program");
+
+            programSet = false;
+            programState = false;
+
+            // Enable Runtime
+            //Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+
+            if (argument == "Set")
+            {
+                SetSystem();
+            }
+            else if (argument == "Reset" && programState)
+            {
+                Echo("Reset");
+            }
+            else if (argument == "Stop" && programState)
+            {
+                programSet = false;
+                programState = false;
+                foreach (IMyPistonBase p in XPistons)
+                {
+                    p.Retract();
+                    pristonXExtract = false;
+                }
+                Runtime.UpdateFrequency = UpdateFrequency.None;
+            }
+            else if (argument == "Start" && programSet)
+            {
+
+                Echo("----------");
+                Echo("Starting");
+                Echo("--------------");
+                programState = true;
+            }
+            else if (programSet && programState)
+            {
+                Echo("----------");
+                Echo("Running");
+                Echo("--------------");
+                AxisJobs();
+            }
+            else if (programSet)
+            {
+                Echo("----------");
+                Echo("Setup Done - Ready to Start!");
+                Echo("--------------");
+            }
+            else
+                Echo("Setup Program First");
+        }
+
+        public void SetSystem()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            programState = false;
+
+            Echo("----------");
+            Echo("SetSystem");
+            Echo("----------");
+
+            //Piston Setup
+            //PVelocity = -0.1F;
+            pristonXExtract = false;
+            pristonZExtract = false;
+
+            PVelocity = -1.5F;
+            PMinLimit = 0;
+            PMaxLimit = 10;
+
+            XPistons = new List<IMyPistonBase>();
+            ZPistons = new List<IMyPistonBase>();
+
+            blocks = new List<IMyTerminalBlock>();
+
+            GridTerminalSystem.SearchBlocksOfName(main_tag, blocks);
+
+            try
+            {
+                foreach (IMyTerminalBlock b in blocks)
+                {
+                    if (b is IMyPistonBase)
+                    {
+                        piston = b as IMyPistonBase;
+
+                        if (piston.CustomName.Contains(XTag))
+                        {
+                            if (!piston.GetValueBool("ShareInertiaTensor"))
+                            {
+                                piston.GetActionWithName("ShareInertiaTensor").Apply(piston);
+                            }
+                            piston.Velocity = PVelocity;
+                            piston.MinLimit = PMinLimit;
+                            piston.MaxLimit = PMaxLimit;
+                            piston.Enabled = true;
+                            XPistons.Add(piston);
+                        }
+                        else if (piston.CustomName.Contains(ZTag))
+                        {
+                            if (!piston.GetValueBool("ShareInertiaTensor"))
+                            {
+                                piston.GetActionWithName("ShareInertiaTensor").Apply(piston);
+                            }
+                            piston.Velocity = PVelocity;
+                            piston.MinLimit = PMinLimit;
+                            piston.MaxLimit = PMaxLimit;
+                            piston.Enabled = true;
+                            ZPistons.Add(piston);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Echo(ex.Message);
+            }
+            programSet = true;
+        }
+
+        public void CheckPistonPos()
+        {
+            var XPos = XPistons[0].CurrentPosition;
+            var XCountPos = XPos * XPistons.Count;
+
+            Echo("---X---");
+            Echo(XCountPos.ToString() + " m");
+            Echo("---Z---");
+            var blocks = XCountPos / 1.5;
+            Echo("---Pistons---");
+            Echo("X piston Count: " + XPistons.Count);
+            Echo("Z piston Count: " + ZPistons.Count);
+            Echo("---Blocks---");
+            Echo(blocks.ToString() + " Blocks");
+        }
+
+        public void PistonSwitch(string coord)
+        {
+            if (coord == "x")
+            {
+                foreach (IMyPistonBase p in XPistons)
+                {
+                }
+            }
+        }
+
+        public void AxisJobs()
+        {
+            Echo("---Jobs Pistons---");
+            Echo("---X---");
+            XAxisJob();
+            Echo("---Z---");
+            ZAxisJob();
+        }
+
+        public void XAxisJob()
+        {
+            var xCurrentPosition = XPistons[0].CurrentPosition;
+
+            if (!pristonXExtract && (xCurrentPosition == XPistons[0].MinLimit))
+            {
+                pristonXExtract = !pristonXExtract;
+                //Extend
+                foreach (IMyPistonBase p in XPistons)
+                {
+                    p.Extend();
+                }
+            }
+            else if (pristonXExtract && (xCurrentPosition == XPistons[0].MaxLimit))
+            {
+                pristonXExtract = !pristonXExtract;
+                //Retract
+                foreach (IMyPistonBase p in XPistons)
+                {
+                    p.Retract();
+                }
+            }
+
+            Echo("X - Piston Count: " + XPistons.Count);
+            Echo("X - Current Pos : " + xCurrentPosition);
+        }
+
+        public void ZAxisJob()
+        {
+            var zCurrentPosition = ZPistons[0].CurrentPosition;
+
+            if (!pristonZExtract && (zCurrentPosition == ZPistons[0].MinLimit))
+            {
+                pristonZExtract = !pristonZExtract;
+                //Extend
+                foreach (IMyPistonBase p in ZPistons)
+                {
+                    p.Extend();
+                }
+            }
+            else if (pristonZExtract && (zCurrentPosition == ZPistons[0].MaxLimit))
+            {
+                pristonZExtract = !pristonZExtract;
+                //Retract
+                foreach (IMyPistonBase p in ZPistons)
+                {
+                    p.Retract();
+                }
+            }
+
+            Echo("Z - Piston Count: " + ZPistons.Count);
+            Echo("Z - Current Pos : " + zCurrentPosition);
+        }
+
+        //******************************************************************
+    }
+}
---
+++ ProgramRef.cs
@@ -0,0 +1,213 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class ProgramRef : MyGridProgram
+    {
+        //******************************************************************
+
+        //Tag Setup
+        // -- Main
+        string main_tag = "Ex/";
+        // Piston Pistions
+        string XTag = "X/";
+
+        //Piston Variables
+        float PVelocity;
+        float PMinLimit;
+        float PMaxLimit;
+
+        bool PistonXJob;
+
+        bool PistonXExtend;
+
+        bool canStart = false;
+        bool programRunning = false;
+
+        IMyPistonBase piston;
+
+        //Piston Setup
+        List<IMyTerminalBlock> blocks;
+
+        int XPistonMax = 0;
+        List<IMyPistonBase> XPistons;
+
+        public ProgramRef()
+        {
+            Echo("Program");
+            // Enable Runtime
+            //Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            Echo("Main");
+
+            if (argument == "Start" && canStart)
+            {
+                Echo("Start!");
+                if (PistonXJob == false)
+                {
+                    InitialStartSystem();
+                }
+
+            }
+            else if (argument == "Set")
+            {
+                Echo("Setting Up!");
+                SetSystem();
+            }
+            else if (argument == "Stop")
+            {
+                programRunning = false;
+                Runtime.UpdateFrequency = UpdateFrequency.None;
+            }
+            else if (argument == "PrintX" && canStart)
+            {
+                int count = 0;
+                foreach (IMyPistonBase pis in XPistons)
+                {
+                    count++;
+                }
+                try
+                {
+                    Echo("Piston Count: " + count);
+                }
+                catch (Exception ex)
+                {
+
+                    Echo("Print Error: " + ex);
+                }
+
+            }
+            if (programRunning)
+            {
+                CheckPistonPistions();
+            }
+        }
+
+        public void SetSystem()
+        {
+            //Setup Bools
+            PistonXJob = false;
+            PistonXExtend = false;
+
+            //Piston Setup
+            //PVelocity = -0.1F;
+            PVelocity = -5.0F;
+            PMinLimit = 0;
+            PMaxLimit = 10;
+
+            Echo("Busy Setting Up...");
+            XPistonMax = 0;
+            XPistons = new List<IMyPistonBase>();
+            blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.SearchBlocksOfName(main_tag, blocks);
+
+            foreach (IMyTerminalBlock b in blocks)
+            {
+                if (b is IMyPistonBase)
+                {
+                    piston = b as IMyPistonBase;
+
+                    if (piston.CustomName.Contains(XTag))
+                    {
+                        if (!piston.GetValueBool("ShareInertiaTensor"))
+                        {
+                            piston.GetActionWithName("ShareInertiaTensor").Apply(piston);
+                        }
+                        piston.Velocity = PVelocity;
+                        piston.MinLimit = PMinLimit;
+                        piston.MaxLimit = PMaxLimit;
+                        XPistons.Add(piston);
+                    }
+                }
+            }
+
+            XPistonMax = XPistons.Count * 6;
+            Echo("Done Setting Up!");
+            Echo("Piston Count: " + XPistons.Count);
+            Echo("X - Max = " + XPistonMax + " m");
+            canStart = true;
+        }
+
+        public void CheckPistonPistions()
+        {
+            var XPos = XPistons[0].CurrentPosition;
+            var XCountPos = XPos * XPistons.Count;
+
+            Echo("---X---");
+            Echo(XCountPos.ToString() + " m");
+            Echo("---Z---");
+            /*
+                var blocks = XCountPos / 2.5;
+                Echo("---Blocks---");
+                Echo(blocks.ToString() + " Blocks");
+            */
+        }
+
+        public void InitialStartSystem()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            PistonXJob = true;
+            PistonSwitch("x");
+        }
+
+        public void PistonGoals()
+        {
+            var xPos = XPistons[0].CurrentPosition;
+
+            if (PistonXJob)
+            {
+                if (xPos == 10 && PistonXExtend)
+                {
+                    PistonSwitch("x");
+                }
+                else if (xPos == 0 && !PistonXExtend)
+                {
+                    PistonSwitch("x");
+                }
+            }
+        }
+
+        public void PistonSwitch(string coord)
+        {
+            if(coord == "x")
+            {
+                foreach (IMyPistonBase p in XPistons)
+                {
+                    if(PistonXExtend)
+                    {
+                        p.Extend();
+                        PistonXExtend = true;
+                    }
+                    else
+                    {
+                        p.Retract();
+                        PistonXExtend = false;
+                    }
+                }
+            }
+        }
+
+        //******************************************************************
+    }
+}
